1df772ee6c6427017627629b1461108658447700
Sobakiin <wgreen16@my.bcit.ca>
2021-03-11 18:26:56 -0800

    Admin users have access to the admin page.

--- models/userModel.js
+++ models/userModel.js
@@ -4,18 +4,21 @@ const database = [
     name: "Jimmy Smith",
     email: "jimmy123@gmail.com",
     password: "jimmy123!",
+    isAdmin: false
   },
   {
     id: 2,
     name: "Johnny Dollar",
     email: "johnny123@gmail.com",
     password: "johnny123!",
+    isAdmin: true
   },
   {
     id: 3,
     name: "Jonathan Chen",
     email: "jonathan123@gmail.com",
     password: "jonathan123!",
+    isAdmin: false
   },
 ];
 
@@ -37,6 +40,13 @@ const userModel = {
   createuser: (profile) => {
     let new_user = {'id': profile.id, 'name':  profile.name}
     database.push(new_user)
+  },
+  isAdmin: (id) =>{
+    const user = database.find((user) => user.id === id)
+    if(user.status === "admin"){
+      return true
+    }
+    return null
   }
 };
 
